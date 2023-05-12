6e68e50056e560d34b2c526254f7a9fe3701311d
ghassewooof <121681428+ghassewooof@users.noreply.github.com>
2023-05-12 13:21:57 +0100

    Add files via upload

---
+++ attack_on_titan.py
@@ -0,0 +1,20 @@
+print("this project for attack on titan fans")
+
+x = int(input("how many part in aot 8 or 7 or 6 \n > "))
+
+if (x == 6):
+    print("go watch attack on titan now dumb")
+elif (x == 7):
+    print("go watch attack on titan now dumb")
+elif (x == 8):
+    z = input("do you want more question y/n \n > ")
+    if (z == 'y' or z == 'Y'):
+        c = input("What is the royal family fritz or yeager or akraman \n > ")
+        if (c == 'yeager'):
+                print("go watch attack on titan now dumb")
+        elif (c == 'fritz'):
+                print('you are rell attack on titan fans')
+        elif(c == 'akraman'):
+                print("go watch attack on titan now dumb")
+    if(z == 'n'):
+        quit()
